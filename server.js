d0fb31cae0899aee294497c1349e72d4ed021d12
Alan Magelssen <alanmagelssen@gmail.com>
2018-10-31 11:27:21 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./'));
+
+app.get('/', (request, response) => {
+  response.sendFile('index.html', {root: './'});
+});
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
