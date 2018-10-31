2c76cdaf124370465243350ff2844f4f413f3b2d
Alan Magelssen <alanmagelssen@gmail.com>
2018-10-31 14:41:52 -0700

    add client code

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+const request = require('superagent');
 const app = express();
+
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./'));
+app.use(cors());
 
 app.get('/', (request, response) => {
   response.sendFile('index.html', {root: './'});
 });
+
+app.get('/location', (request, response) => {
+  const locationData = searchToLatLng(request.query.data);
+  response.send(locationData);
+});
+
+function searchToLatLng(query) {
+  // call geo api from google
+  const location = new Location();
+  return location;
+}
+
+function Location(data) {
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+
+}
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
